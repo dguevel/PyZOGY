8f299768a893c15d8be703e0876b090426cba5ea
David Guevel <guevel.david@gmail.com>
2017-11-09 10:34:35 -0500

    changed to builtin unittest module

--- PyZOGY/test/test_util.py
+++ PyZOGY/test/test_util.py
@@ -1,17 +1,17 @@
 import unittest as ut
 import PyZOGY.util as util
-import mock_image_class
+from . import mock_image_class
 import numpy as np
 
 class test_util(ut.TestCase):
 
-    def test_center_psf():
-        psf = np.zeros((50, 51))
+    def test_center_psf(self):
+        psf = np.zeros((50, 51)
         psf[25, 25] = 1
         assert(util.center_psf(psf)[0, 0] == 1)
 
 
-    def test_make_mask():
+    def test_make_mask(self):
         saturation = 1.
         image = mock_image_class.MockImageClass(shape=(50, 50), saturation=saturation)
         image[25, 25] = 2.
@@ -23,7 +23,8 @@ class test_util(ut.TestCase):
         mask = util.mask_saturated_pix(image, image.saturation, None)
         assert(mask[image >= saturation].all() == True)
 
-    def test_interpolate_bad_pixels():
+
+    def test_interpolate_bad_pixels(self):
         image = mock_image_class.MockImageClass(shape=(50, 50))
         image[0, 25] = np.nan
         image[25, 0] = np.nan
