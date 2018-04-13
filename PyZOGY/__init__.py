ddaedcda221d32eca180ce1f04d5482c38728ea8
david <guevel.david@gmail.com>
2018-04-12 23:49:41 -0400

    transitioned more functions to objected oriented format

--- PyZOGY/__init__.py
+++ PyZOGY/__init__.py
@@ -1,6 +1,6 @@
-import numpy
+import numpy as np
 
-class BaseImage(numpy.ma.MaskedArray):
+class BaseImage(np.ndarray):
     """
     Base class for difference imaging.
 
@@ -23,9 +23,9 @@ class BaseImage(numpy.ma.MaskedArray):
 
     def __new__(cls, data, **kwargs):
 
-        obj = numpy.ma.MaskedArray(data, **kwargs)
+        obj = np.asarray(data, **kwargs).view(cls)
         obj._fft = None
-        obj._fft_pad = None
+        obj._pad = None
 
         return obj
 
@@ -33,39 +33,39 @@ class BaseImage(numpy.ma.MaskedArray):
     def __array_finalize__(self, obj):
         if obj is None:
             return
-        obj._fft = getattr(obj, '_fft', None)
-        obj._fft_pad= getattr(obj, '_pad', None)
+        self._fft = getattr(obj, '_fft', None)
+        self._pad= getattr(obj, '_pad', None)
 
 
     def fft(self):
         """
         Calculate the object's padded FFT.
 
-        Use numpy to pad and calculate the FFT. This function stores the FFT
+        Use np to pad and calculate the FFT. This function stores the FFT
         in memory to avoid repeated calculation. If you don't want this, use
-        numpy.fft directly on the instance.
+        np.fft directly on the instance.
 
         Returns
         -------
-        fft : numpy.ma.MaskedArray
+        fft : np.ma.MaskedArray
             Fourier transformed masked array.
         """
 
         if self._fft is None:
-            self._fft = numpy.ma.MaskedArray(numpy.fft.fft2(self.pad()), mask=self.mask)
+            self._fft = np.fft.fft2(self.pad()).real
         return self._fft
 
 
-    def fft_pad(self):
+    def pad(self):
         """
         Pad an array to a power of two.
         
-        Use numpy to pad the edges to the next highest power of two to 
+        Use np to pad the edges to the next highest power of two to 
         speed up FFT and to avoid effects of circular convolution.
 
         Returns
         -------
-        fft_pad : numpy.ma.MaskedArray
+        fft_pad : np.ma.MaskedArray
             Padded masked array.
         """
 
@@ -73,14 +73,13 @@ class BaseImage(numpy.ma.MaskedArray):
             constant = np.median(self)
             n = 0
             defecit = [0, 0]
-            while (data.shape[0] > (2 ** n)) or (data.shape[1] > (2 ** n)):
+            while (self.shape[0] > (2 ** n)) or (self.shape[1] > (2 ** n)):
                 n += 1
-                defecit = [(2 ** n) - data.shape[0], (2 ** n) - data.shape[1]]
-            padding = ((0, defecit[0]), (0, defecit[1])),
-            padded_data = numpy.pad(self, padding, mode='constant', constant_values=constant)
-            padded_mask = numpy.pad(self.mask, padding, mode='constant', constant_values=True)
-            self._fft_pad = numpy.ma.MaskedArray(padded_mask, mask=padded_mask)
-        return self._fft_pad
+                defecit = [(2 ** n) - self.shape[0], (2 ** n) - self.shape[1]]
+            padding = ((0, defecit[0]), (0, defecit[1]))
+            padded_data = np.pad(self, padding, mode='constant', constant_values=constant)
+            self._pad = padded_data
+        return self._pad
 
 
 class PSF(BaseImage):
@@ -96,14 +95,12 @@ class PSF(BaseImage):
     def __array_finalize__(self, obj):
         if obj is None:
             return
-        obj._fft = getattr(obj, '_fft', None)
-        obj._fft_pad= getattr(obj, '_pad', None)
 
     def resize(self, shape):
         """
         Resize centered psf to larger shape.
 
-        Use numpy to resize psf to a larger shape for convolution with image.
+        Use np to resize psf to a larger shape for convolution with image.
         
         Parameters
         ----------
@@ -112,7 +109,7 @@ class PSF(BaseImage):
 
         Returns
         -------
-        extended : numpy.ndarray
+        extended : np.ndarray
             Padded PSF.
         """
 
@@ -128,10 +125,149 @@ class PSF(BaseImage):
             self._extended = np.pad(psf, padding, mode='constant', constant_values=0.)
         return psf_extended
 
+    def mask_saturated_pix(self):
+        """Make a pixel mask that marks saturated pixels; optionally join with input_mask"""
+
+        input_mask = np.zeros(image.shape)
+
+        input_mask[image >= self.saturation] = 1
+        input_mask = input_mask.astype(bool)
+
+
+def pad_to_power2(data, value='median'):
+
 class Image(BaseImage):
-    pass
+    def __new__(cls, data, psf, mask=None, **kwargs):
+
+        obj = BaseImage(data, **kwargs).view(cls)
+        obj.zero_point = np.inf
+        obj.psf = psf
+        obj.background_std = np.inf
+        
+        if mask = None:
+            obj.mask = np.zeros(obj.shape)
+        
+        return obj
+
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self.zero_point = getattr(obj, 'zero_point', np.inf)
+        self.psf = getattr(obj, 'psf', None)
+        self.background_std= getattr(obj, 'background_std', None)
+
+
+
+    def difference_image(self, reference, gain_mask=None, use_pixels=False, show=False, percent=99):
+        """
+        Calculate the difference image using the Zackay algorithm.
+        
+        This is the main function that calculates the difference image using the 
+        Zackey, Ofek, Gal-Yam 2016. It operates on ImageClass objects defined in
+        image_class.py. The function will fit the gain ratio if not provided. 
+        Ultimately this calculates equation 13 in Zackey, Ofek, Gal-Yam 2016.
+        
+        Parameters
+        ----------
+        reference : PyZOGY.Image
+            ImageClass instance created from the reference image.
+        gain_mask : str or numpy.ndarray, optional
+            Array or FITS file holding an array of pixels to use when fitting
+            the gain ratio.
+        use_pixels : bool, optional
+            Fit the gain ratio using pixels (True) or stars (False) in image.
+        show : bool, optional
+            Display debuggin plots during fitting.
+        percent : float, optional
+            Percentile cutoff to use for fitting the gain ratio. 
+
+        Returns
+        -------
+        difference_image : numpy.ndarray
+            The difference between science and reference images.
+        """
+
+        # match the gains, set reference to their one, self to 1.
+        if self.zero_point == np.inf or reference.zero_point == np.inf:
+            if gain_mask is not None:
+                self.mask[gain_mask == 1] = 1
+                reference.mask[gain_mask == 1] = 1
+            self.zero_point = util.solve_iteratively(science, reference, use_pixels=use_pixels, show=show, percent=percent)
+            reference.zero_point = 1.
+
+        # calculate difference image
+        denominator = self.background_std ** 2 * reference.zero_point ** 2 * abs(reference.psf.fft()) ** 2
+        denominator += reference.background_std ** 2 * self.zero_point ** 2 * abs(self.psf.fft()) ** 2
+        difference_image_fft = self.fft() * reference.psf.fft() * reference.zero_point
+        difference_image_fft -= reference.image.fft() * self.psf.fft() * self.zero_point
+        difference_image_fft /= np.sqrt(denominator)
+        difference_image = np.fft.ifft2(difference_image_fft)
+
+        difference = DifferenceImage(difference_image, self, reference)
+
+        return difference
 
 class DifferenceImage(BaseImage):
+    def zero_point(self):
+        """
+        Calculate the flux based zero point of the difference image.
+        
+        Calculate the difference image flux based zero point using equation 15 of 
+        Zackey, Ofek, Gal-Yam 2016.
+
+        Parameters
+        ----------
+        science : PyZOGY.ImageClass
+            ImageClass instance created from the science image.
+        reference : PyZOGY.ImageClass
+            ImageClass instance created from the reference image.
+
+        Returns
+        -------
+        difference_image_zero_point : float
+            Flux based zero point of the difference image.
+        """
+
+        denominator = self.science.background_std ** 2 * self.reference.zero_point ** 2
+        denominator += self.reference.background_std ** 2 * self.science.zero_point ** 2
+        zero_point = self.science.zero_point * self.reference.zero_point / np.sqrt(denominator)
+
+        logging.info('Global difference image zero point is {}'.format(np.mean(difference_image_zero_point)))
+        return zero_point
+
+
+    def psf(self):
+        """
+        Calculate the PSF of the difference image.
+        
+        Calculactes the PSF of the difference image using equation 17 of Zackey,
+        Ofek, Gal-Yam 2016.
+
+        Parameters
+        ----------
+        science : PyZOGY.ImageClass
+            ImageClass instance created from the science image.
+        reference : PyZOGY.ImageClass
+            ImageClass instance created from the reference image.
+        difference_image_zero_point : float
+            Flux based zero point of the difference image.
+
+        Returns
+        -------
+        difference_psf : numpy.ndarray
+            PSF of the difference image.
+        """
+
+        denominator = self.science.background_std ** 2 * self.reference.zero_point ** 2 * abs(self.reference.psf.fft()) ** 2
+        denominator += reference.background_std ** 2 * science.zero_point ** 2 * abs(self.science.psf.fft) ** 2
+
+        psf_fft = science.zero_point * science_psf_fft * reference_psf_fft
+        psf_fft /= self.zero_point * np.sqrt(denominator)
+        psf = PSF(np.fft.ifft2(psf_fft))
+
+        return psf
+
     pass
 
 class ScoreImage(BaseImage):
