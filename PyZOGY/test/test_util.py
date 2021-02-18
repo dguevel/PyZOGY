7cc4308bf825af3e77fc3d23b8a7cabe8885d9d4
Griffin Hosseinzadeh <g.hosseinzadeh90@gmail.com>
2021-02-17 17:39:00 -0800

    reformat test_util.py

--- PyZOGY/test/test_util.py
+++ PyZOGY/test/test_util.py
@@ -3,13 +3,13 @@ import PyZOGY.util as util
 from . import mock_image_class
 import numpy as np
 
+
 class test_util(ut.TestCase):
 
     def test_center_psf(self):
-        psf = np.zeros((50, 51)
+        psf = np.zeros((50, 51))
         psf[25, 25] = 1
-        assert(util.center_psf(psf)[0, 0] == 1)
-
+        assert util.center_psf(psf)[0, 0] == 1
 
     def test_make_mask(self):
         saturation = 1.
@@ -18,11 +18,10 @@ class test_util(ut.TestCase):
         image[30, 30] = 1.
 
         mask = util.mask_saturated_pix(image, image.saturation, np.zeros(image.shape))
-        assert(mask[image >= saturation].all() == True)
+        assert mask[image >= saturation].all()
 
         mask = util.mask_saturated_pix(image, image.saturation, None)
-        assert(mask[image >= saturation].all() == True)
-
+        assert mask[image >= saturation].all()
 
     def test_interpolate_bad_pixels(self):
         image = mock_image_class.MockImageClass(shape=(50, 50))
@@ -32,5 +31,4 @@ class test_util(ut.TestCase):
         image.mask = np.zeros(shape=(50, 50))
         image.mask[np.isnan(image)] = 1
         image = util.interpolate_bad_pixels(image)
-        assert(np.isfinite(image).all())
-
+        assert np.isfinite(image).all()
